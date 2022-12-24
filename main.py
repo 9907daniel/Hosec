da2f29fb40ee0c98fb9b22971750d0d685122c36
Michelle Choi <michellechoi@00.local>
2022-12-24 12:00:55 +0900

    initial commit

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+import cv2
+import numpy as np
 
-def test():
-    pass
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    
+    image = np.zeros(frame.shape, np.uint8)
+    smaller_frame = cv2.resize(frame, (0, 0), fx = 0.5, fy = 0.5)
+    
+    cv2.imshow('frame', frame)
+    
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
+
+
+
+
+
+
+
+
